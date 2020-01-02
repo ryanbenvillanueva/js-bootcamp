8a0f1496bce1ad4e8038a6b2658c170875aadcd0
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-02 19:44:32 +0800

    Complete challenge 5 - boolean.js

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,3 +1,10 @@
+let temp = 32;
+let isFreezing = temp <= 32;
+
+if (isFreezing) {
+    console.log('It is freezing outside!');
+}
+
 // Challenge area
 
 // Create age set to your age
@@ -7,8 +14,15 @@
 // Print is senior value
 
 let age = 39;
-let isChild = age <= 7;
-let isSenior = age >= 65;
 
-console.log(isChild);
-console.log(isSenior);
+// if 7 or under print message about child pricing
+
+if (age <= 7) {
+    console.log('You get a child discount!');
+}
+
+if (age >= 65) {
+    console.log('You get a senior discount!');
+}
+
+// if 65 or older print message about senior discount
