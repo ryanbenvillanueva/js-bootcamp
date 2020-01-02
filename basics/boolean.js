12dd43280139692b61a1159913f54135373f9ffe
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-02 11:30:36 +0800

    Complete challenge 4 - boolean.js

---
+++ basics/boolean.js
@@ -0,0 +1,14 @@
+// Challenge area
+
+// Create age set to your age
+// Calculate is child - if they are 7 or under
+// Calculate is senior - if they are 65 or older
+// Print is child value
+// Print is senior value
+
+let age = 39;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+
+console.log(isChild);
+console.log(isSenior);
