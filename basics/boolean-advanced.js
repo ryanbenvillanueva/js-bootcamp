70d4d1605d0e9b381c2c8df5df5ccd9054d957a9
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-02 20:02:14 +0800

    Complete challenge 6 - boolean-advanced.js

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,24 @@
+let isAccountLocked = true;
+let userRole = 'admin';
+
+if (isAccountLocked) {
+    console.log('is account locked');
+} else {
+    console.log('Welcome!');
+}
+
+// Challenge area
+
+let temp = 45;
+
+if (temp <=32) {
+    console.log("It is freezing outside!");
+} else if (temp < 110) {
+    console.log("Go for it. It is pretty nice.");
+} else {
+    console.log("It is hot outside!");
+}
+
+// It is freezing outside!
+// It is hot outside!
+// Go for it. It is pretty nice.
