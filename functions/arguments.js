7f7a8068a2533ecc59f4e1cb783adc3f87109179
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-03 19:24:28 +0800

    Complete challenge 10 - arguments.js

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result);
 
 // Default arguments
 let getScoreText = function (name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score;
+    // return 'Name: ' + name + ' - Score: ' + score;
+    return `Name: ${name} - Score: ${score}`;
 }
 
 let scoreText = getScoreText();
@@ -16,12 +17,12 @@ console.log(scoreText);
 
 // Challenge area
 // total, tipPercent .2
+// A 25% tip on $40 would be $10.
 
 let getTip = function (total, tipPercent = .2) {
-    return total * tipPercent;
+    return `A 25% tip on $${total} would be $${total * tipPercent}`;
 }
 
-let tipOne = getTip(100, undefined);
-let tipTwo = getTip(100, .3);
-console.log(tipOne);
-console.log(tipTwo);
+let tip = getTip(40, .25);
+console.log(tip);
+
