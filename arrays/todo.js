ae448a3290158288a659c05abc22920366306f7e
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-07 14:43:48 +0800

    Solve challenge 20 - todo.js

--- arrays/todo.js
+++ arrays/todo.js
@@ -3,8 +3,14 @@
 const todos = ['Order Cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise' ];
 
 todos.splice(2, 1);
-todos.push('This is the new last item');
+todos.push('Buy coffee');
 todos.shift();
 
 console.log(`You have ${todos.length} todos!`);
-console.log(todos);
+
+// 1. The first item
+// 2. The second item
+
+todos.forEach(function (todo, index) {
+    console.log(`${index + 1}. ${todo}`);
+});
