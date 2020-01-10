a569846e150f9443203d67a632a9b67f42ce4d29
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-01-10 10:51:12 +0800

    Solve challenge 34 - todo-app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -16,24 +16,28 @@ const todos = [{
 }];
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    hideCompleted: false
 };
 
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+        return searchTextMatch && hideCompletedMatch;
     });
 
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.completed;
     });
-    
+
     document.querySelector('#todos').innerHTML = '';
 
     const summary = document.createElement('h2');
     summary.textContent = `You have ${incompleteTodos.length} todos left`;
     document.querySelector('#todos').appendChild(summary);
-    
+
     filteredTodos.forEach(function (todo) {
         const p = document.createElement('p');
         p.textContent = todo.text;
@@ -57,4 +61,9 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
     });
     renderTodos(todos, filters);
     e.target.elements.text.value = '';
+});
+
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
 });
